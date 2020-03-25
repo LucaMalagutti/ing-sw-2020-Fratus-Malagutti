ec666471363ea23f28dd11694518ad90d37c40ae
LucaMalagutti <malagutti.luca@yahoo.it>
2020-03-25 19:30:04 +0100

    Initial commit

---
+++ src/main/java/it/polimi/ingsw/PSP4/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.PSP4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Test" );
+    }
+}
